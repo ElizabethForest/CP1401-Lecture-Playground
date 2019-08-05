a3307359a3436be8c1ff2f3e670e89a486522e34
ElizabethForest <elizabeth.forest@my.jcu.edu.au>
2019-08-05 10:03:17 +1000

    lecture 2

---
+++ lecture_2.py
@@ -0,0 +1,11 @@
+# Get the first number from the user, convert it into an integer and save it to a variable called a
+a = int(input("please enter a number"))
+
+# Get the second number from the user, convert it into an integer and save it to a variable called b
+b = int(input("please enter a second number"))
+
+# Get the sum of a and b and save it into a variable called total
+total = a + b
+
+# Display the total
+print("The sum of those is " + str(total))
