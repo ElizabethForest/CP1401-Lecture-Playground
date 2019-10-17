58ffd66135e21593ed1207b3627a86842f354e6f
ElizabethForest <elizabeth.forest@my.jcu.edu.au>
2019-10-17 11:14:35 +1000

    update pig latin

--- lecture_10_activity.py
+++ lecture_10_activity.py
@@ -1,20 +1,19 @@
 # activity 1 - pig latin
 # TODO add more comments explaining whats happening
+VOWELS = "aeiou"
 
 
 def main():
     word = input("enter a word").lower()
-    vowels = "aeiou"
-
     while word != '.':
-        if word[0] in vowels:
+        if word[0] in VOWELS:
             pig_latin = word + "yay"
 
         else:
             # TODO include other possible methods
             count = 0
             for char in word:
-                if char not in vowels:
+                if char not in VOWELS:
                     count += 1
                 else:
                     break
@@ -24,6 +23,40 @@ def main():
         word = input("enter a word")
 
     print("Thanks for playing")
+
+
+def translate_1(word):
+    """ This function develops the pig latin word by keeping count of the current index until the first vowel is reached
+    and then using the index to split the string """
+    count = 0
+    for char in word:
+        if char not in VOWELS:
+            count += 1
+        else:
+            return word[count:] + word[:count] + "ay"
+
+
+def translate_2(word):
+    """ This function keeps track of both sections of the word, removing from the beginning of the word and adding to
+    the result until the first letter of the word is a vowel"""
+    result = ""
+
+    while len(word) != 0 and word[0] not in VOWELS:
+        result += word[0]
+        word = word[1:]
+
+    return word + result + "ay"
+
+
+def translate_3(word):
+    """ This function works very similarly to translate_1 but instead of keeping track of the current index uses the
+    .index() function to find the first occurrence of a vowel """
+
+    for char in word:
+        if char in VOWELS:
+            first_vowel = word.index(char)
+            return word[first_vowel:] + word[:first_vowel] + "ay"
+
 
 
 main()
